cf84e06ffb8180471aab6f4360c1c46850baf535
Fabio Cermelli <f.cermelli94@gmail.com>
2018-06-04 18:02:55 +0200

    Ended train module

--- network.py
+++ network.py
@@ -1,22 +1,14 @@
-import numpy as np
-import math
-import torch
 import torch.nn as nn
-from torchvision.models import ResNet,DenseNet
-from collections import OrderedDict
-from torchvision import models
-import torch.utils.model_zoo as model_zoo
-import copy
-import torch.nn.functional as F
+
 
 # Define a residual block
 class BasicBlock(nn.Module):
-    def __init__(self, block, inplanes, planes, stride=1, first=False):
+    def __init__(self, block, inplanes, planes, kernel_size=3, stride=1, first=False):
         super(BasicBlock, self).__init__()
-        self.conv1 = block(inplanes, planes, kernel_size=3, stride=stride)
+        self.conv1 = block(inplanes, planes, kernel_size=kernel_size, stride=stride)
         self.bn1 = nn.BatchNorm2d(planes)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = block(planes, planes, kernel_size=3)
+        self.conv2 = block(planes, planes, kernel_size=kernel_size)
         self.bn2 = nn.BatchNorm2d(planes)
 
         if first:
@@ -26,17 +18,17 @@ class BasicBlock(nn.Module):
         self.first = first
 
     def forward(self, x):
-        if first:
+        if self.first:
             residual = self.downsample(x)
-        else :
+        else:
             residual = x
-            
+
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
         out = self.conv2(out)
         out = self.bn2(out)
-        
+
         out += residual
         out = self.relu(out)
 
@@ -50,34 +42,34 @@ class WideResNet(nn.Module):
         self.block = block
         self.in_channel = 16
         
-        self.conv1 = block(3, self.in_channel, kernel_size=3, stride=1, padding=1)
+        self.conv1 = block(3, self.in_channel, kernel_size=kernel_size, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(self.in_channel)
         
         self.relu = nn.ReLU()
-        self.layer1 = self._make_layer_(resnet_block, 64, widening_factor, stride=2)
-        self.layer2 = self._make_layer_(resnet_block, 128, widening_factor, stride=2)
-        self.layer3 = self._make_layer_(resnet_block, 256, widening_factor, stride=2)
+        self.layer1 = self._make_layer_(resnet_block, 64, kernel_size, widening_factor, stride=2)
+        self.layer2 = self._make_layer_(resnet_block, 128, kernel_size, widening_factor, stride=2)
+        self.layer3 = self._make_layer_(resnet_block, 256, kernel_size, widening_factor, stride=2)
         
         self.avgpool = nn.AdaptiveAvgPool2d(1)
         
-        self.fc = nn.ModuleList([nn.Linear(256,classes)]) #initialize with one class
+        self.fc = nn.ModuleList([nn.Linear(256, classes)])  # initialize with one class
         self.index = 0
         
-    def set_index (self, index):
-        if( index < len(self.fc)):
+    def set_index(self, index):
+        if index < len(self.fc):
             self.index = index
                                  
     def add_task(self, module):
         self.fc.append(module)
-        return len(self.fc) - 1 #return actual index of the added module
+        return len(self.fc) - 1  # return actual index of the added module
         
-    def _make_layer_(self, block, planes, blocks, stride=1):
+    def _make_layer_(self, block, planes, kernel_size, blocks, stride=1):
         strides = [stride] + [1]*(blocks-1)
         layers = []
         
         for i in range(0, blocks):
-            layers.append(block(self.block, self.in_channel, planes, stride=strides[i], first=(i==0)))
-            self.inplanes = planes
+            layers.append(block(self.block, self.in_channel, planes,kernel_size, stride=strides[i], first=(i == 0)))
+            self.in_channel = planes
 
         return nn.Sequential(*layers)
     
@@ -94,7 +86,8 @@ class WideResNet(nn.Module):
         x = self.fc[self.index](x)
         return x
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     net = WideResNet(nn.Conv2d, BasicBlock, widening_factor=4, classes=1000)
     print(net)
     dict(net.named_parameters()).keys()
--- training.py
+++ training.py
@@ -1,3 +1,8 @@
+import torch
+import torch.optim as optim
+import torch.nn as nn
+from torch.autograd import Variable
+from torchvision import transforms, datasets
 
 # Training settings
 PATH_TO_DATASETS='....'
@@ -14,13 +19,209 @@ WORKERS=8
 IMAGENET_MEAN = [0.485, 0.456, 0.406]
 IMAGENET_STD = [0.229, 0.224, 0.225]
 
+# Dataset to index dict
+DICT_NAMES = {'imagenet12': 0,
+            'aircraft': 1, 'cifar100': 2,
+            'daimlerpedcls': 3, 'dtd': 4,
+            'gtsrb': 5, 'omniglot': 6,
+            'svhn': 7, 'ucf101': 8,
+            'vgg-flowers': 9}
 
-#Training steps:
-    # Preprocessing (cropping, hor-flipping, resizing)
+
+# Check for CUDA usage
+cuda = not NO_CUDA and torch.cuda.is_available()
+
+def train(model, dataset_name, prefix, bn=False, mirror=True, scaling=True, decay=0.0, adamlr=0.0001, lr=0.001, momentum=0.9 ):
+    # Training steps:
+    # Preprocessing (cropping, hor-flipping, resizing) and get data
+    # Initialize data processing threads
+    workers = WORKERS if cuda else 0
+
+    data_transform = get_data_transform(mirror, scaling)
+
+    dataset = datasets.ImageFolder(root=PATH_TO_DATASETS + '/' + dataset_name + '/train',
+                                   transform=data_transform)
+    train_loader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=workers)
+
+    # Build the test loader
+    # (note that more complex data transforms can be used to provide better performances e.g. 10 crops)
+    data_transform = transforms.Compose([
+        transforms.CenterCrop(IMAGE_CROP),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD)
+    ])
+
+    dataset = datasets.ImageFolder(root=PATH_TO_DATASETS + '/' + dataset_name + '/val',
+                                   transform=data_transform)
+    test_loader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=workers)
+
+    ignored_params = list(map(id, model.fc.parameters()))
+    base_params = filter(lambda p: id(p) not in ignored_params, model.parameters())
     # set optimizer
+    optimizerA = optim.Adam(base_params, lr=adamlr, weight_decay=decay)
+    optimizerB = optim.SGD(model.fc.parameters(), lr=lr, momentum=momentum, weight_decay=decay)
+    schedulerA = optim.lr_scheduler.StepLR(optimizerA, STEP)
+    schedulerB = optim.lr_scheduler.StepLR(optimizerB, STEP)
+    scheduler = MultipleOptimizer(schedulerA,schedulerB)
+    optimizer = MultipleOptimizer(optimizerA, optimizerB)
+
     # set loss function
+    cost_function = nn.CrossEntropyLoss()
     # perform training epochs time
+    if cuda:
+        model = model.cuda()
+
+    loss_epoch_min = -1
+    for epoch in range(1, EPOCHS + 1):
+        scheduler.step()
+        loss_epoch = train_epoch(model, epoch, train_loader, optimizer, cost_function, bn)
+        if loss_epoch_min == -1 :
+            loss_epoch_min = loss_epoch
+        result = test_epoch(model, test_loader, dataset_name, cost_function)
+
+        print('Train Epoch: {} \tTrainLoss: {:.6f} \tTestLoss: {:.6f}\tAccuracyTest: {:.6f}'.format(
+            epoch, loss_epoch, result[0], result[1]))
+
+        # Save the model
+        if loss_epoch < loss_epoch_min:
+            save_checkpoint({
+                'epoch': epoch,
+                'state_dict': model.state_dict(),
+                'optimizerA': optimizerA.state_dict(),
+                'optimizerB': optimizerB.state_dict()
+            }, prefix + "_checkpoint.pth")
 
 
-# Multi Optimizer
-#https://discuss.pytorch.org/t/two-optimizers-for-one-model/11085/6
+# Perform a single training epoch
+def train_epoch(model, epoch, train_loader, optimizers, cost_function, bn=False):
+    # Set the model in training mode
+    model.train()
+
+    # If BN parameters must be freezed, freeze them
+    if not bn:
+        print("Deactivating BN")
+        for m in model.modules():
+            if isinstance(m, nn.BatchNorm1d) or isinstance(m, nn.BatchNorm2d):
+                m.eval()
+    # Init holders
+    losses = 0
+    current = 0
+
+    # Perform the training procedure
+    for batch_idx, (data, target) in enumerate(train_loader):
+
+        # Move the variables to GPU
+        if cuda:
+            data, target = data.cuda(), target.cuda()
+
+        # Reset the optimizers
+        optimizers.zero_grad()
+
+        # Process input
+        output = model(data)
+
+        # Compute loss and gradients
+        loss = cost_function(output, target)
+        loss.backward()
+
+        # Update parameters
+        optimizers.step()
+
+        # Check for log and update holders
+        if batch_idx % LOG_INTERVAL == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                       100. * batch_idx / len(train_loader), loss.data[0]))
+
+        losses += loss.item()*data.size(0)
+        current += data.size(0)
+
+    return losses / current
+
+
+def test_epoch(model, test_loader, dataset, cost_function):
+    # Put the model in eval mode
+    model.eval()
+    torch.set_grad_enabled(False)
+    # Init holders
+    test_loss = 0
+    correct = 0
+
+    # Perform the evaluation procedure
+    for data, target in test_loader:
+        if cuda:
+            data, target = data.cuda(), target.cuda()
+
+        output = model(data)
+
+        # Update holders
+        test_loss += cost_function(output, target).data[0]  # sum up batch loss
+        pred = torch.max(output, 1)[1]  # get the index of the max log-probability
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()  # Check if the prediction is correct
+
+    # Compute accuracy and loss
+    total_loss = test_loss / len(test_loader.dataset)
+    accuracy = 100. * correct / len(test_loader.dataset)
+
+    print('Dataset ' + dataset + ' : Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
+        total_loss, correct, len(test_loader.dataset),
+        accuracy))
+
+    results = [accuracy, total_loss]
+
+    torch.set_grad_enabled(True)
+    return results
+
+
+# Save the current checkpoint
+def save_checkpoint(state, checkpoint):
+    torch.save(state, checkpoint)
+
+
+def get_data_transform(mirror, scaling):
+    # Create Dataloader w.r.t. chosen transformations
+    if mirror:
+        if scaling:
+            data_transform = transforms.Compose([
+                transforms.RandomResizedCrop(IMAGE_CROP),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD)
+            ])
+        else:
+            data_transform = transforms.Compose([
+                transforms.RandomCrop(IMAGE_CROP),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD)
+            ])
+    else:
+        if scaling:
+            data_transform = transforms.Compose([
+                transforms.RandomResizedCrop(IMAGE_CROP),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD)
+            ])
+        else:
+            data_transform = transforms.Compose([
+                transforms.RandomCrop(IMAGE_CROP),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD)
+            ])
+    return data_transform
+
+
+class MultipleOptimizer(object):
+    # Multi Optimizer
+    #   https://discuss.pytorch.org/t/two-optimizers-for-one-model/11085/6 '''
+
+    def __init__(self, *op):
+        self.optimizers = op
+
+    def zero_grad(self):
+        for op in self.optimizers:
+            op.zero_grad()
+
+    def step(self):
+        for op in self.optimizers:
+            op.step()
